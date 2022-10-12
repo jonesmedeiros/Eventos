a5898f8b54f9c7a1370d7b122b3ddeb0f2736dd6
Jones Roberto Medeiros Dias <jones.robertomedeirosdias@gmail.com>
2022-10-11 23:29:57 -0300

    Alterando Controllers

--- Back/src/Eventos.API/Controllers/WeatherForecastController.cs
+++ Back/src/Eventos.API/Controllers/WeatherForecastController.cs
@@ -9,31 +9,18 @@ namespace Eventos.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class EventoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+      
+        public EventoController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+          
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+           return  "value";
         }
     }
 }
